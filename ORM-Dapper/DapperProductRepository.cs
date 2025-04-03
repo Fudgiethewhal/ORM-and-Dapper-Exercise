ad450ae49bc6415bb8d36230216ccded645e12c7
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-04-02 23:23:54 -0700

    completed the bonus material

--- ORM-Dapper/DapperProductRepository.cs
+++ ORM-Dapper/DapperProductRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Text;
 using System.Data;
-using MySql.Data.MySqlClient;
+
 
 namespace ORM_Dapper;
 
@@ -20,6 +20,31 @@ public class DapperProductRepository : IProductRepository
     
     public IEnumerable<Product> GetAllProducts()
     {
-        return _conn.Query<Product>("SELECT * FROM product");
+        return _conn.Query<Product>("SELECT * FROM product;");
+    }
+
+    public Product GetProduct(int id)
+    {
+        return _conn.QuerySingle<Product>("SELECT * FROM product WHERE productID = @id;",
+            new { id });
+        }
+
+    public void UpdateProduct(Product product)
+    {
+        _conn.Execute("UPDATE products" +
+                      "Set Name = @name," +
+                      "Price= @price," +
+                      "CategoryID = @catID," +
+                      "OnSale = @onSale," +
+                      "StockLevel = @stock" +
+                      "WHERE productID = @id;",
+            new
+            {
+                name = product.Name,
+                price = product.Price,
+                catID = product.CategoryID,
+                OnSale = product.OnSale,
+                stock = product.StockLevel
+            });
     }
 }
