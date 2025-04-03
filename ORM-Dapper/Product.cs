217052be6ccf57809b800a4418267758b57a61d3
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-04-02 22:06:22 -0700

    added product class

---
+++ ORM-Dapper/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace ORM_Dapper;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int CategoryId { get; set; }
+    public bool OnSale { get; set; }
+    public int Stocklevel { get; set; }
+    
+    
+}
